c9007d1e0aaecbd734d62399b3a01dfb6d682422
ian root <ianaroot@gmail.com>
2013-11-20 23:03:16 -0800

    finish sad path for user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Hartl::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signin/', to: 'sessions#new'
+  match 'signout', to: 'sessions#destroy'
 
   root to: "static_pages#home"
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
 
 
   it { should be_valid }
@@ -119,5 +120,10 @@ describe User do
       @user.save
       @user.reload.email.should == uppercase_email.downcase
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
