95837a2ec96847d261b0c4c4f877f613505530ab
ian root <ianaroot@gmail.com>
2013-11-21 19:34:29 -0800

    add dropdown for profile, settings, and signout to header

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,7 +26,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email", with: user.email.upcase
-        fill_in "Password", with: user.Password
+        fill_in "Password", with: user.password
         click_button "Sign in"
       end
 
