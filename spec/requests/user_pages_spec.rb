587787d2584b1c9db997035767c0dc9fdd4c80f2
ian root <ianaroot@gmail.com>
2013-11-24 16:32:56 -0800

    refactor tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,8 +7,8 @@ describe "UserPages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_selector('h1', text: "Sign up") }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_heading("Sign up") }
+    it { should have_title(full_title('Sign up')) }
 
   end
 
@@ -16,8 +16,8 @@ describe "UserPages" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_selector('h1',     text: user.name) }
-    it { should have_selector('title',  text: user.name) }
+    it { should have_heading(user.name) }
+    it { should have_title(user.name) }
   end
 
   describe "signup" do
@@ -32,24 +32,21 @@ describe "UserPages" do
       describe "after submission" do
         before { click_button submit }
 
-        it { should have_selector('title', text: 'Sign up') }
+        it { should have_title('Sign up') }
         it { should have_content('error') }
-        it { should have_selector('div.alert', text: 'The form contains 6 errors') }
-        it { should have_selector('li', text: "Name can't be blank") }
-        it { should have_selector('li', text: "Email can't be blank") }
-        it { should have_selector('li', text: "Email is invalid") }
-        it { should have_selector('li', text: "Password can't be blank") }
-        it { should have_selector('li', text: "Password is too short (minimum is 6 characters") }
-        it { should have_selector('li', text: "Password confirmation can't be blank") }
+        it { should have_error_message('The form contains 6 errors') }
+        it { should list_error("Name can't be blank") }
+        it { should list_error("Email can't be blank") }
+        it { should list_error("Email is invalid") }
+        it { should list_error("Password can't be blank") }
+        it { should list_error("Password is too short (minimum is 6 characters") }
+        it { should list_error("Password confirmation can't be blank") }
       end
     end
 
     describe "with valid information" do
       before do
-        fill_in "Name",     with: "example user"
-        fill_in "Email",    with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        valid_signup
       end
 
       it "should create a user" do
@@ -60,8 +57,8 @@ describe "UserPages" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
 
-        it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_title(user.name) }
+        it { should have_success_message('Welcome') }
         it { should have_link('Sign out') }
       end
     end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,8 +6,41 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def valid_signup
+        fill_in "Name",     with: "example user"
+        fill_in "Email",    with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+end  
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
   end
 end
+
+RSpec::Matchers.define :have_title do |text|
+  match do |page|
+    page.should have_selector('title', text: text)
+  end
+end
+
+
+RSpec::Matchers.define :have_heading do |heading|
+  match do |page|
+    page.should have_selector('h1', text: heading)
+  end
+end
+
+RSpec::Matchers.define :list_error do |message|
+  match do |page|
+    page.should have_selector('li', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
