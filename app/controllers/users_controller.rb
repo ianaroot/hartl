16847a13604b24b9bc88870b1a291246fb2951d1
ian root <ianaroot@gmail.com>
2013-11-25 23:48:37 -0500

    secure update and edit actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only:  [:edit, :update]
+  before_filter :correct_user, only:    [:edit, :update]
+
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    # @user = User.find(params[:id])
-  #   flash[:success] = "Update successful"
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -35,4 +34,14 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  private
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
