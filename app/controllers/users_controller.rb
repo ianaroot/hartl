aa56c4c080266065876e66de18112809730591a9
ian root <ianaroot@gmail.com>
2013-11-26 22:32:53 -0500

    create admins, allow admins to delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only:  [:index, :edit, :update]
+  before_filter :signed_in_user, only:  [:index, :edit, :update, :destroy]
   before_filter :correct_user, only:    [:edit, :update]
+  before_filter :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -11,6 +12,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_url
   end
 
   def update
@@ -50,5 +57,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
